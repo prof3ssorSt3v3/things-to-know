be88f57e81ca644cc517d461c5109a371689fefc
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2020-02-24 11:55:49 -0500

    update

---
+++ gitme.js
@@ -0,0 +1,22 @@
+const { exec } = require('child_process');
+
+//console.log(process.argv);
+let args = process.argv;
+args.splice(0, 2);
+let message = args.join(' ');
+console.log(message);
+
+exec('git add .', (err, stdout, stdin) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('We have added');
+});
+exec(`git commit -m "${message}"`, (err, stdout, stdin) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('message applied');
+});
